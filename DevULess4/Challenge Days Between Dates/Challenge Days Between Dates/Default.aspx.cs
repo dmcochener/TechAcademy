821fcab5a602b09bfa624d1bf76d03b43c7a1e71
Deanna <deanna.cochener@gmail.com>
2017-07-10 13:32:04 -0700

    Uploaded DevU Lesson 4

---
+++ DevULess4/Challenge Days Between Dates/Challenge Days Between Dates/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Challenge_Days_Between_Dates
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            DateTime day1 = firstCalendar.SelectedDate;
+            DateTime day2 = secondCalendar.SelectedDate;
+            TimeSpan difference;
+
+            if (day1 > day2)
+                difference = day1 - day2;
+            else
+                difference = day2 - day1;
+
+            resultLabel.Text = difference.TotalDays.ToString();
+        }
+    }
+}
