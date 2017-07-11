ec143af562c94f25ec9c551ddb575e5a741e3b43
Deanna <deanna.cochener@gmail.com>
2017-07-10 19:14:04 -0700

    Uplaoded DevU Lesson 6

---
+++ DevULess6/ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Wolverine fewest battles
+            // Pheonix most battles
+
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            string result = "";
+
+            // Your Code Here!
+            int highest = numbers.Max();
+            int lowest = numbers.Min();
+
+            for (int index = 0; index < names.Length; index++)
+            {
+                if (numbers[index] == highest)
+                {
+                    result += String.Format("Most battles belong to: {0} (Value: {1}) <br />", names[index], highest);
+                }
+
+                if (numbers[index] == lowest)
+                {
+                    result += String.Format("Least battles belong to: {0} (Value: {1}) <br />", names[index], lowest);
+                }
+            }
+
+            resultLabel.Text = result;
+        }
+    }
+}
---
+++ DevULess6/EpicSpiesAssetTracker/EpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" Height="150px" ImageUrl="~/epic-spies-logo.jpg" />
+            <br />
+            <h1>Asset Performance Tracker</h1>
+            <br />
+            Asset Name:&nbsp;
+            <asp:TextBox ID="assetNameTB" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Elections Rigged:
+            <asp:TextBox ID="electionsTB" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="subterfugeTB" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Asset" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
