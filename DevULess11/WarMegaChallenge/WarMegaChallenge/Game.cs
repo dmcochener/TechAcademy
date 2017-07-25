463adc6c48fe9af945ff92b32750b85f2dc8c1ee
Deanna <deanna.cochener@gmail.com>
2017-07-25 12:31:33 -0700

    Completed War Game Code
    Code could be refactored, could add play with new decks, doesn't handle battle if too few cards left.

--- DevULess11/WarMegaChallenge/WarMegaChallenge/Game.cs
+++ DevULess11/WarMegaChallenge/WarMegaChallenge/Game.cs
@@ -31,8 +31,21 @@ namespace WarMegaChallenge
                 if (player1Draw.Value == player2Draw.Value)
                 {
                     result += "This means WAR!<br />";
-                    break;
-                    //DoBattle (_player1, _player2);
+                    Card player1Contender;
+                    Card player2Contender;
+                    do
+                    {
+                        Deck battleDeck1 = StartBattle(_player1);
+                        battleDeck1.Cards.Add(player1Draw);
+                        Deck battleDeck2 = StartBattle(_player2);
+                        battleDeck2.Cards.Add(player2Draw);
+                        bounty = moveCards(battleDeck1, battleDeck2, _player1, _player2);
+                        player1Contender = battleDeck1.Cards[1];
+                        player2Contender = battleDeck2.Cards[1];
+                    }
+                    while (player1Contender.Value == player2Contender.Value);
+
+                    GetResults(player1Contender, player2Contender, _player1Win, _player2Win, bounty);
                 }
                 else
                     GetResults(player1Draw, player2Draw, _player1Win, _player2Win, bounty);
@@ -41,18 +54,18 @@ namespace WarMegaChallenge
 
             }
 
-            result += String.Format("<br />{0} has {1} cards <br />{2} has {3} cards <br />", _player1Win.Owner.Name, _player1Win.Cards.Count,
+            result += String.Format("<br /><br />{0} has {1} cards <br />{2} has {3} cards <br />", _player1Win.Owner.Name, _player1Win.Cards.Count,
                 _player2Win.Owner.Name, _player2Win.Cards.Count);
 
         }
 
-        //NEED TO ADD CODE TO THE WAR!!!
-
-        private void DoBattle(Deck player1, Deck player2)
+        private Deck StartBattle(Deck _current)
         {
-            List<Card> player1Cards = BattleCards(player1);
-            List<Card> player2Cards = BattleCards(player2);
-
+            List<Card> playerCards = BattleCards(_current);
+            result += String.Format("{0} draws a {1} of {2}, a {3} of {4}, and a {5} of {6}<br />",
+                _current.Owner.Name, playerCards[0].Name, playerCards[0].Suit, playerCards[1].Name, playerCards[1].Suit,
+                playerCards[2].Name, playerCards[2].Suit);
+            return new Deck(_current.Owner, playerCards);
         }
 
         private void GetResults(Card player1Draw, Card player2Draw, Deck _player1Win, Deck _player2Win, List<Card> bounty)
@@ -81,7 +94,24 @@ namespace WarMegaChallenge
             return bounty;
         }
 
-        private void moveCards(List<Card> player1Draw, List<Card> player2Draw, Deck _player1, Deck _player2, List<Card> _bounty)
+        private List<Card> moveCards(Deck _battleDeck1, Deck _battleDeck2, Deck _player1, Deck _player2)
+        {
+            List<Card> bounty = new List<Card>();
+            foreach (var card in _battleDeck1.Cards)
+            {
+                _player1.Cards.Remove(card);
+                bounty.Add(card);
+            }
+            foreach (var card in _battleDeck2.Cards)
+            {
+                _player2.Cards.Remove(card);
+                bounty.Add(card);
+            }
+
+            return bounty;
+        }
+
+        private List<Card> moveCards(List<Card> player1Draw, List<Card> player2Draw, Deck _player1, Deck _player2, List<Card> _bounty)
         {
             foreach (var card in player1Draw)
             {
@@ -94,6 +124,7 @@ namespace WarMegaChallenge
                 _bounty.Add(card);
             }
 
+            return _bounty;
         }
 
         private void AddBounty(Deck _winner, List<Card> bounty)
@@ -112,8 +143,19 @@ namespace WarMegaChallenge
 
         private List<Card> BattleCards(Deck _current)
         {
+            Card card1;
+            Card card2;
+            Card card3;
+            do
+            {
+                card1 = DrawCard(_current);
+                card2 = DrawCard(_current);
+                card3 = DrawCard(_current);
+            }
+            while ((card1 == card2) || (card2 == card3) || (card1 == card3));
+
             return new List<Card>()
-            { DrawCard(_current), DrawCard(_current), DrawCard(_current)};
+            { card1, card2, card3};
         }
     }
 
